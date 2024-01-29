f5b7523f6330ea7d8ea32364af649d8643c603e8
tukiyubiitai <tukiyubiitai@gmail.com>
2024-01-29 12:10:14 +0900

    MapApi通信

--- lib/repository/map_repository.dart
+++ lib/repository/map_repository.dart
@@ -1,7 +1,7 @@
 import 'dart:convert' as convert;
 import 'dart:convert';
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 
