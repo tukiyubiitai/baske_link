beabaaf402c8fd77609d1b8e099f27ad7a890706
tukiyubiitai <tukiyubiitai@gmail.com>
2024-01-29 12:16:42 +0900

    状態管理

--- lib/state/providers/account/account_notifier.dart
+++ lib/state/providers/account/account_notifier.dart
@@ -1,8 +1,10 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../../models/account/account.dart';
+
 part 'account_notifier.g.dart';
 
+//アカウント情報を管理
 @Riverpod(keepAlive: true)
 class AccountNotifier extends _$AccountNotifier {
   @override
